d3795d3667111b270ea2547f3bc6a30c0d714e91
Geert Huls <geert.huls82@gmail.com>
2023-10-15 22:44:30 +0200

    Named queue example

---
+++ EasyNetqMessages/CardPaymentNamedQueue.cs
@@ -0,0 +1,12 @@
+﻿using EasyNetQ;
+
+namespace EasyNetqMessages;
+
+[Queue("CardPaymentQueue", ExchangeName = "CardPaymentExchange")]
+public class CardPaymentNamedQueue
+{
+    public string CardNumber { get; set; } = default!;
+    public string CardHolderName { get; set; } = default!;
+    public string ExpiryDate { get; set; } = default!;
+    public decimal Amount { get; set; } = default!;
+}
