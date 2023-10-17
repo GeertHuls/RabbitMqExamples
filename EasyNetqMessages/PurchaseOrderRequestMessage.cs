79adfc5dcbd11b79a32ed37144d4be814c95c5ae
Geert Huls <geert.huls82@gmail.com>
2023-10-17 23:26:33 +0200

    Send and receive examples

---
+++ EasyNetqMessages/PurchaseOrderRequestMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyNetqMessages;
+
+public class PurchaseOrderRequestMessage
+{
+    public string PoNumber { get; set; } = default!;
+    public string CompanyName { get; set; } = default!;
+    public int PaymentDayTerms { get; set; } = default!;
+    public decimal Amount { get; set; } = default!;
+}
---
+++ Receive/Program.cs
@@ -0,0 +1,24 @@
+﻿using EasyNetQ;
+using EasyNetqMessages;
+using static EasyNetqMessages.Constants;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using var bus = RabbitHutch.CreateBus(RabbitMqConnectionString);
+        bus.SendReceive.Receive<CardPaymentRequestMessage>("my.paymentsqueue", message => HandleCardPaymentMessage(message));
+
+        Console.WriteLine("Listening for messages. Hit <return> to quit.");
+        Console.ReadLine();
+    }
+
+    static void HandleCardPaymentMessage(CardPaymentRequestMessage paymentMessage)
+    {
+        Console.WriteLine("Processing Payment = <" +
+                          paymentMessage.CardNumber + ", " +
+                          paymentMessage.CardHolderName + ", " +
+                          paymentMessage.ExpiryDate + ", " +
+                          paymentMessage.Amount + ">");
+    }
+}
